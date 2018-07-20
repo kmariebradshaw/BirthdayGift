106eb9739c995b63c005a99182b89a02e9a15843
Kaitlyn Bradshaw <kaitlynbradshaw@Blairs-MacBook-Pro-2.local>
2018-07-20 17:58:07 -0500

    please

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,8 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-
+config.serve_static_assets = true
+config.assets.compile = true
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -89,13 +90,11 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 # Disable Rails's static asset server (Apache or nginx will already do this)  
-config.serve_static_assets = false
 
 # Compress JavaScripts and CSS  
 config.assets.compress = true
 
 # Don't fallback to assets pipeline if a precompiled asset is missed  
-config.assets.compile = false
 
 # Generate digests for assets URLs  
 config.assets.digest = true
